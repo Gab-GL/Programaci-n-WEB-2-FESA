35a668916cd97a38ffa06e8948330dd024b9bf2e
Gab-GL <143673065+Gab-GL@users.noreply.github.com>
2026-03-30 09:49:33 -0600

    Add files via upload

---
+++ Objetos en JS/objetos.js
@@ -0,0 +1,50 @@
+let alumno = {
+    id: 321149170,
+    nombre: "Gabril",
+    primerApellido: "Gonzalez",
+    segundoApellido: "Lopez",
+    edad: 20,
+    titulado: false,
+    egresado: {
+        estado: false
+    },
+    domicilio: {
+        calle: {
+            callejon: "colima",
+            numero: 7,
+            
+        },
+        numero: "24B",
+        colonia: "San Lorenzo",
+        municipio: "Coacalco",
+        estado: "EDO MEX",
+        pais: "México",
+        continenete: "Latinoamérica"
+    },
+    kinder: {
+        nombre: "EDmundo de amsis",
+        actividadPrimerdia: function () {
+            console.log("correr");
+        },
+        actividadRecurrente: function () {
+            console.log("cantar canciones");
+        },
+        datosMiss: {
+            nombre: "Paola",
+            edad: 29,
+            estudios: "Docorado"
+        }
+    },
+    primaria: {
+        nombre: "Benito Juárez",
+        comer(comida) {
+            return `ahora está comiendo ${comida}`;
+        },
+        mensaje(mensajeAlumno) {
+            return `${this.nombre} es la Primaria y el alumno tiene que ir a ${mensajeAlumno}`;
+        }
+    }
+}
+console.log(alumno.kinder.datosMiss.nombre);
+console.log(alumno.primaria.mensaje("Biblioteca"));
+console.log(alumno.primaria.comer("Manzana"));
